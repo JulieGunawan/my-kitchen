0a4f011c66af3a66a743d1019685f89e169ea9ab
Julie Gunawan <julie.gunawan@protonmail.com>
2023-04-13 06:28:47 -0600

    utilize useParam from react-router-dom to pull individual blog

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,14 +1,16 @@
 import '../App.css';
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const ArticlePage = () =>{
+  //params is an object whose keys are the name of the url parameter eg :articleId in app.js
+  //the key is :articleId and the value is the json value from article-content
+  const params = useParams();
+  const articleId = params.articleId;
     return(
         <div>
-          <ul>
-            <li>Mix and Match</li>
-            <li>Desserts</li>
-          </ul>
+          <h1>This is article page with id {articleId}</h1>
         </div>
       );
 }
---
+++ src/pages/article-content.js
@@ -0,0 +1,17 @@
+const article = [
+    {
+        name:'first-blog',
+        title:'My First Blog Here',
+        content: [
+            yada yada yada 
+        ]
+    },
+    {
+        name:"second-blog",
+        title:"My Second Blog",
+        content:[
+            bla bla bla
+        ]
+    },
+]
+export default article;
