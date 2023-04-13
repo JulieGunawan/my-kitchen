2d7ff3ad3da625f8b82192b0a077973f56c99432
Julie Gunawan <julie.gunawan@protonmail.com>
2023-04-13 06:42:33 -0600

    listing all blogs' title and the first 150 characters into the blog page

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,15 +1,23 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import articles from './article-content';
 
 const Blogs = () => {
     return (
-      <div>
+      <>
         <h1>Welcome to my blogs</h1>
         <p>In this page, I will be discussing 
           about how to be more creative when it comes to food.
           Food is life, thus we need to enjoy it.</p>
-      </div>
+        {articles.map(article => (
+          <div>
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)}</p>
+          </div>
+        ))}
+
+      </>
     );
 }
 
